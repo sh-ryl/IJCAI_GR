d07b1faa041810c629547774e33a01b7544666b3
Sheryl Mantik <S3693591@student.rmit.edu.au>
2024-09-17 11:34:22 +1000

    new model for skills and belief

--- scenario.py
+++ scenario.py
@@ -69,7 +69,7 @@ scenarios["default"]["hidden_items"] = [
 scenarios["train"] = {}
 
 scenarios["train"]["goal_sets"] = [
-    {"axe": 1, "bridge": 0.1}
+    {"iron": 0.7, "wood": 1, "grass": -1}
 ]
 
 scenarios["eval"] = {}

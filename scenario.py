6a2aeea5d9a8ff7dee9c9d2220efb97568cf731d
Sheryl Mantik <S3693591@student.rmit.edu.au>
2024-10-02 15:42:09 +1000

    added new models

--- scenario.py
+++ scenario.py
@@ -76,12 +76,13 @@ scenarios["train"]["goal_sets"] = [
     # {"iron": 0.7, "wood": 1, "grass": -1}
     # {"cloth": 0.7, "stick": 0.3}
     # {"plank": 0.1, "stick": 0.9}
-    {"axe": 1, "bridge": 0.1}
+    # {"axe": 1, "bridge": 0.1}
+    {"gem": 0, "gold": 0}
 ]
 
 scenarios["eval"] = {}
 scenarios["eval"]["goal_sets"] = [
-    {"iron": 0.7, "wood": -1, "grass": 1}
+    # {"iron": 0.7, "wood": -1, "grass": 1}
     # {"gem": 0.7, "gold": 0.3}
     # {"axe": 1, "bridge": 0.5}
     # {"cloth": 0.9, "stick": 0.1}
